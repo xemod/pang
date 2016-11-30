e13d5e16e9ddeade4c1e682f8f16ce67f67e59a2
xemod <wichanan@gmail.com>
2016-11-30 09:20:08 +0700

    test

--- scriptlocal.js
+++ scriptlocal.js
@@ -1,3 +1,3 @@
 function onDownload() {
-	document.location.href = 'proposal.docx';
+	document.location.href = 'https://www.hsri.or.th/sites/all/modules/contrib/pubdlcnt/pubdlcnt.php?file=/sites/default/files/attachment/proposal.docx&nid=7535';
 }
